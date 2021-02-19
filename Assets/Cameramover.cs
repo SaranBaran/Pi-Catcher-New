09b146d48cdec23b9c92bd3aa204d44a956b47e6
yavuz <yavuz.celik.05@gmail.com>
2021-02-19 23:39:31 +0300

    we have gravity but no prediction

---
+++ Assets/Cameramover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameramover : MonoBehaviour
+{
+    Vector3 lastpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetMouseButton(2))
+        {
+            transform.position += lastpos - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        lastpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
