3fe4539862a093f244a63b61603d11f6ae3a7e48
SaranBaran <69807636+SaranBaran@users.noreply.github.com>
2021-02-20 23:55:05 +0300

    start menu ve güneş animasyonu

---
+++ Assets/startMenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class startMenuManager : MonoBehaviour
+{
+
+    public string scenes;
+
+    public void startScenes()
+    {
+        SceneManager.LoadScene(scenes);
+        Debug.Log("Start");
+    }
+
+}
