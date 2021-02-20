ce1d93954526f6d63e8b34d564ec72b308a091c1
SaranBaran <69807636+SaranBaran@users.noreply.github.com>
2021-02-21 01:13:48 +0300

    benden bugünlük bu kadar hadi hayırlısı

--- Assets/RoverManager.cs
+++ Assets/RoverManager.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoverManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    public Transform checkpoint;
+    [SerializeField]
+    public Text distanceTexts;
+    private float distance;
+    public Animator anim;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //distance text için
+        distance = (checkpoint.transform.position - transform.position).magnitude;
+
+        distanceTexts.text = "Distance: " + distance.ToString("F1") + " meters";
+
+        if (distance <= 0)
+        {
+            distanceTexts.text = "Landed!";
+        }
     }
+
+    public void OnTriggerEnter2D(Collider2D Planet)
+    {
+        if (Planet.tag == "notMars")//mars olmayan her şey için
+        {
+            anim.SetBool("Crash", true);
+            Debug.Log("test");
+        }
+    }
+
 }
