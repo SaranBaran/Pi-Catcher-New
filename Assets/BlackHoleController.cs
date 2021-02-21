e8c2e5af4fe45510ddcce550d4f798a68efea37e
SaranBaran <69807636+SaranBaran@users.noreply.github.com>
2021-02-21 16:17:48 +0300

    güçük bir hata varmış oops

--- Assets/BlackHoleController.cs
+++ Assets/BlackHoleController.cs
@@ -17,7 +17,7 @@ public class BlackHoleController : MonoBehaviour
             GetComponent<SpriteRenderer>().color = GetComponent<CelestialBody>().isactive ? activeclolor : PassiveColor;
             if (GetComponent<CelestialBody>().isactive)
             {
-                karadelikSound.Start();
+                karadelikSound.Play();
             }
             else
             {
