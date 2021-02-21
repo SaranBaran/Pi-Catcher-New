fc303fd730ea9e7fb4c85bad86bfb7251feb1e59
yavuz <yavuz.celik.05@gmail.com>
2021-02-21 14:49:42 +0300

    prefab is a prefab

--- Assets/RoverManager.cs
+++ Assets/RoverManager.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class RoverManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.collider.gameObject.name == "MARS")
+        {
+
+        }
     }
 }
